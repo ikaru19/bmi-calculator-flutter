d8b85b19d8aa8101e9695fa8135f425d9f2bb7d6
ikaru19 <syafrizalizal17@gmail.com>
2020-05-23 11:23:46 +0700

    add customize slider

---
+++ lib/constans.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const kLabelTextSize = TextStyle(
+    fontSize: 18.0,
+    color: Color(0xFF8D8E98));
+
+const kNumberTextStyler = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w400,
+);
+
+const kBottomContainerHeight = 80.0;
+const kActiveColour = Color(0xFF1D1E33);
+const kBottomColour = Color(0xFFEB1555);
+const kInactiveColour = Color(0xFF111328);
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reuseable_card.dart';
+import 'icon_content.dart';
+import 'constans.dart';
+
+enum Gender {
+  male,
+  female,
+}
 
 class InputPage extends StatefulWidget {
   @override
@@ -6,6 +16,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  Gender selectedGender;
+  int height = 150;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,63 +26,117 @@ class _InputPageState extends State<InputPage> {
           title: Text('BMI CALCULATOR'),
         ),
         body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
                     child: ReuseableCard(
-                      colour: Color(0xFF1D1E33),
+                      onPress: () {
+                        setState(() {
+                          selectedGender = Gender.male;
+                        });
+                      },
+                      cardChild: IconContent(
+                        icon: FontAwesomeIcons.mars,
+                        label: "MALE",
+                      ),
+                      colour: selectedGender == Gender.male
+                          ? kActiveColour
+                          : kInactiveColour,
                     ),
                   ),
                   Expanded(
-                    child: ReuseableCard(
-                      colour: Color(0xFF1D1E33),
-                    )
-                  ),
+                      child: ReuseableCard(
+                    onPress: () {
+                      setState(() {
+                        selectedGender = Gender.female;
+                      });
+                    },
+                    cardChild: IconContent(
+                      icon: FontAwesomeIcons.venus,
+                      label: "FEMALE",
+                    ),
+                    colour: selectedGender == Gender.female
+                        ? kActiveColour
+                        : kInactiveColour,
+                  )),
                 ],
               ),
             ),
             Expanded(
               child: ReuseableCard(
-                colour: Color(0xFF1D1E33),
-              )
+                colour: kActiveColour,
+                cardChild: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'HEIGHT',
+                      style: kLabelTextSize,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.baseline,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          Text(
+                            height.toString(),
+                            style: kNumberTextStyler
+                          ),
+                          Text(
+                            'cm',
+                            style: kLabelTextSize,
+                          ),
+
+                        ],
+                    ),
+                    SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        activeTrackColor: Colors.white,
+                        thumbColor: Color(0xFFEB1555),
+                        overlayColor:Color(0x15EB1555) ,
+                        thumbShape: RoundSliderThumbShape(
+                            enabledThumbRadius: 15.0
+                        ),
+                        overlayShape: RoundSliderOverlayShape(
+                          overlayRadius: 30.0
+                        )
+                      ),
+                      child: Slider(
+                          value: height.toDouble(),
+                          min: 90.0,
+                          max: 220.0,
+                          onChanged: (double newValue){
+                            setState(() {
+                              height = newValue.round();
+                            });
+                          }
+                      ),
+                    )
+                  ],
+                ),
+              ),
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child:ReuseableCard(
-                      colour: Color(0xFF1D1E33),
-                    )
-                  ),
-                  Expanded(
-                    child: ReuseableCard(
-                      colour: Color(0xFF1D1E33),
-                    )
-                  ),
+
+                      child: ReuseableCard(
+                    colour: kActiveColour,
+                  )),
+                  Expanded(child: ReuseableCard(colour: kActiveColour)),
                 ],
               ),
             ),
+            Container(
+              color: kBottomColour,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           ],
         ));
-  }
-}
-
-class ReuseableCard extends StatelessWidget {
-
-  ReuseableCard({@required this.colour});
-
-  Color colour;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    );
   }
 }
